4d31682eccf7434cbb461065b728a765046b0de9
Charles (Chuck) Prystupa <71155568+CharlesP8412@users.noreply.github.com>
2024-09-01 15:40:12 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -2,6 +2,7 @@
 int main() {
 	char last_name[20];
 	printf ("Enter your last name: ");
-	scanf ("%s", last_name);
+	// Originally scanf ("%s", last_name); // No limit
+	scanf ("%19s", last_name);  //Limits to 19
 	printf("Entered last name is %s\n", last_name);
 	}
